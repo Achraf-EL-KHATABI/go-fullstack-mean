50151d4d061c23f01f9e72af758b4c3dbd632286
Achraf EL KHATABI <ac.elkhatabi@gmail.com>
2020-11-22 17:39:42 +0100

    Réalisation de la partie gestion de fichiers

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
-const exprees = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const stuffRoutes = require('./routes/stuff');
 const userRoures = require('./routes/user');
 
-const app = exprees();
+const app = express();
 
 mongoose.connect('mongodb+srv://achraf:Test2020@cluster0.dczv3.mongodb.net/test?retryWrites=true&w=majority',
   {
@@ -23,6 +24,8 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+
+app.use('/images',  express.static(path.join(__dirname, 'images')));
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoures);
